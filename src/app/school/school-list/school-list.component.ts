3c6c3d367aec47309b543383e10ec69e53b55673
M. Nafees Salman Khan <salman.dev.pwd@hotmail.com>
2025-08-31 12:08:03 +0600

    school list input enhanced

--- src/app/school/school-list/school-list.component.ts
+++ src/app/school/school-list/school-list.component.ts
@@ -47,7 +47,7 @@ export class SchoolListComponent implements OnInit {
   pdfContent: any[] = [];
 
   constructor(
-    private schoolService: SchoolService, 
+    private schoolService: SchoolService,
     private prep: PreparationService,
     private cdRef: ChangeDetectorRef,
     private spinnerService: NgxSpinnerService
@@ -59,10 +59,10 @@ export class SchoolListComponent implements OnInit {
 
   loadData(): void {
     this.spinnerService.show();
-    
+
     this.subs.sink = this.schoolService
-      .getListByOfficeByCode("ee_nator")
-      //.getListByOfficeByCode(this.prep.getUserName())
+      // .getListByOfficeByCode("ee_nator")
+      .getListByOfficeByCode(this.prep.getUserName())
       .subscribe((s) => {
         this.spinnerService.hide();
         this.schools = s;
@@ -79,196 +79,201 @@ export class SchoolListComponent implements OnInit {
     return Common.dateToBengali(dt, false);
   }
 
-  view(itemId: number): void {
-    // Implement view logic here
+  isInputComplete(input: boolean): string {
+    return input ? 'হ্যাঁ' : 'না';
+    // return input ? '<span class="text-success">হ্যাঁ</span>' : '<span class="text-danger">না</span>';
   }
 
-  edit(itemId: number): void {
-    // Implement edit logic here
-  }
+  // view(itemId: number): void {
+  //   // Implement view logic here
+  // }
 
-  generatePdf(id: number): void {
-    this.spinnerService.show();
-    this.prepareData();
-    this.createDocument();
-    setTimeout(() => {
-      this.spinnerService.hide();
-      pdfMake.createPdf(this.pdfDefinition).open();
-    }, 1500);
-  }
+  // edit(itemId: number): void {
+  //   // Implement edit logic here
+  // }
 
-  open(){}
+  // generatePdf(id: number): void {
+  //   this.spinnerService.show();
+  //   this.prepareData();
+  //   this.createDocument();
+  //   setTimeout(() => {
+  //     this.spinnerService.hide();
+  //     pdfMake.createPdf(this.pdfDefinition).open();
+  //   }, 1500);
+  // }
 
-  getPdfTableHeader(): any[] {
-    return [
-      [
-        { text: 'ক্রম', style: 'headerTexts' },
-        { text: 'নাম ও পদবী', style: 'headerTexts' },
-        { text: 'বর্তমান কর্মস্থল', style: 'headerTexts' },
-        { text: 'পদায়নকৃত কর্মস্থল', style: 'headerTexts' },
-      ],
-    ];
-  }
+  // open(){}
 
-  prepareData() {
-    this.pdfContent = [];
-    let headerData = this.getPdfTableHeader();
-    headerData.forEach((element) => {
-      this.pdfContent.push(element);
-    });
-    let serial: number = 1;
+  // getPdfTableHeader(): any[] {
+  //   return [
+  //     [
+  //       { text: 'ক্রম', style: 'headerTexts' },
+  //       { text: 'নাম ও পদবী', style: 'headerTexts' },
+  //       { text: 'বর্তমান কর্মস্থল', style: 'headerTexts' },
+  //       { text: 'পদায়নকৃত কর্মস্থল', style: 'headerTexts' },
+  //     ],
+  //   ];
+  // }
 
-   
-  }
+  // prepareData() {
+  //   this.pdfContent = [];
+  //   let headerData = this.getPdfTableHeader();
+  //   headerData.forEach((element) => {
+  //     this.pdfContent.push(element);
+  //   });
+  //   let serial: number = 1;
 
-  createDocument() {
-    this.pdfDefinition = {
-      pageSize: 'A4',
-      // pageOrientation: 'landscape',
-      // watermark: {text:'Public Works Department',color: '#c9c7c7'},
-      info: {
-        title: 'Audit Report',
-      },
-      content: [
-        {
-          text: 'গণপ্রজাতন্ত্রী বাংলাদেশ সরকার',
-          // style: 'header1',
-          alignment: 'center',
-          fontSize: 14,
-        },
-        {
-          // text: 'প্রধান প্রকৌশলীর কার্যালয়\nগণপূর্ত মনিটরিং এন্ড অডিট সার্কেল,\nপূর্ত ভবন, ঢাকা।\nফোনঃ ০২২২৩৩৮২২০৭',
-          text: 'গণপূর্ত অধিদপ্তর\nপূর্ত ভবন, ঢাকা।\nফোনঃ ০২২২৩৩৮২২০৭',
-          style: 'header2',
-          alignment: 'center',
-        },
-        '\n\n',
-        // '\n\nবিষয়ঃ মাসিক সমন্বয় সভার জন্য অডিট আপত্তি সংক্রান্ত তথ্য',
-        // {
-        //   columns: [
-        //     [{ text: 'মাসের নামঃ __________________________________________' }],
-        //     [
-        //       {
-        //         text: 'টাকার পরিমাণঃ লক্ষ টাকায়',
-        //         alignment: 'right',
-        //       },
-        //     ],
-        //   ],
-        //   style: 'lineSpacing12',
-        // },
-        // {text: 'Column/row spans', pageBreak: 'before', style: 'subheader'},
-        // {text: 'Column/row spans', style: 'subheader'},
-        // 'Each cell-element can set a rowSpan or colSpan',
-        {
-          style: 'tableSetup',
-          // color: '#444',
-          // layout: 'lightHorizontalLines',
-          table: {
-            // widths: [15, '*', 25, 40, 25, 40, 25, 40, 25, 25, 25, 40, 30, 30, 30, 30, 40, 40],
-            widths: [30, '*', 150, 150],
-            headerRows: 1,
-            // keepWithHeaderRows: 1,
-            dontBreakRows: true,
 
-            body: this.pdfContent,
-            style: 'lineSpacing24',
-          },
-        },
-        { text: '', style: 'lineSpacing60' },
-      ],
-      // pageMargins: [ this.inchToPointConvert(0.7), this.inchToPointConvert(0.8), this.inchToPointConvert(0.2), this.inchToPointConvert(0.4) ],
-      footer: function (currentPage, pageCount) {
-        return {
-          margin: 0,
-          columns: [
-            {
-              // fontSize: 9,
-              text: [
-                {
-                  text: 'HR Transfer System',
-                  // style: 'footer1',
-                  margin: [2, 0, 0, 0],
-                },
-              ],
-              // alignment: 'center'
-            },
-            {
-              // fontSize: 9,
-              text: [
-                {
-                  text:
-                    pageCount > 1
-                      ? `${currentPage.toString()} of ${pageCount}`
-                      : '',
-                  style: 'footerPageNumber',
-                },
-              ],
-              alignment: 'center',
-            },
-            {
-              fontSize: 9,
-              text: [
-                {
-                  text: '', //new Date().toLocaleDateString(),
-                },
-              ],
-              // alignment: 'center'
-            },
-          ],
-        };
-      },
-      defaultStyle: {
-        font: 'NIKOSH',
-        fontSize: 10,
-      },
-      styles: {
-        lineSpacing12: {
-          margin: [0, 0, 0, 12],
-        },
-        lineSpacing24: {
-          margin: [0, 0, 0, 24],
-          fontSize: 14,
-        },
-        lineSpacing60: {
-          margin: [0, 0, 0, 60],
-        },
-        tableSetup: {
-          margin: [-5, 0, -5, 0],
-          // margin: [0, 5, 0, 15]
-        },
-        headerTexts: {
-          fontSize: 9,
-          alignment: 'center',
-          margin: [0, 2, 0, 0],
-        },
-        // header: {
-        //   fontSize: 9,
-        //   alignment: 'right',
-        //   margin: [0, 5, 0, 0]
-        // },
-        tableTexts: {
-          fontSize: 10,
-          margin: [0, 3, 0, 0],
-        },
-        tableNumbers: {
-          fontSize: 9,
-          alignment: 'right',
-          margin: [0, 3, 0, 0],
-        },
-        footer1: {
-          fontSize: 6,
-          margin: [10, 0, 0, 0],
-          // alignment: 'center'
-        },
-        footerPageNumber: {
-          fontSize: 6,
-          // alignment: 'center'
-        },
-      },
-    };
-  }
+  // }
 
-  inchToPointConvert(num: number) {
-    return num * 72;
-  }
+  // createDocument() {
+  //   this.pdfDefinition = {
+  //     pageSize: 'A4',
+  //     // pageOrientation: 'landscape',
+  //     // watermark: {text:'Public Works Department',color: '#c9c7c7'},
+  //     info: {
+  //       title: 'Audit Report',
+  //     },
+  //     content: [
+  //       {
+  //         text: 'গণপ্রজাতন্ত্রী বাংলাদেশ সরকার',
+  //         // style: 'header1',
+  //         alignment: 'center',
+  //         fontSize: 14,
+  //       },
+  //       {
+  //         // text: 'প্রধান প্রকৌশলীর কার্যালয়\nগণপূর্ত মনিটরিং এন্ড অডিট সার্কেল,\nপূর্ত ভবন, ঢাকা।\nফোনঃ ০২২২৩৩৮২২০৭',
+  //         text: 'গণপূর্ত অধিদপ্তর\nপূর্ত ভবন, ঢাকা।\nফোনঃ ০২২২৩৩৮২২০৭',
+  //         style: 'header2',
+  //         alignment: 'center',
+  //       },
+  //       '\n\n',
+  //       // '\n\nবিষয়ঃ মাসিক সমন্বয় সভার জন্য অডিট আপত্তি সংক্রান্ত তথ্য',
+  //       // {
+  //       //   columns: [
+  //       //     [{ text: 'মাসের নামঃ __________________________________________' }],
+  //       //     [
+  //       //       {
+  //       //         text: 'টাকার পরিমাণঃ লক্ষ টাকায়',
+  //       //         alignment: 'right',
+  //       //       },
+  //       //     ],
+  //       //   ],
+  //       //   style: 'lineSpacing12',
+  //       // },
+  //       // {text: 'Column/row spans', pageBreak: 'before', style: 'subheader'},
+  //       // {text: 'Column/row spans', style: 'subheader'},
+  //       // 'Each cell-element can set a rowSpan or colSpan',
+  //       {
+  //         style: 'tableSetup',
+  //         // color: '#444',
+  //         // layout: 'lightHorizontalLines',
+  //         table: {
+  //           // widths: [15, '*', 25, 40, 25, 40, 25, 40, 25, 25, 25, 40, 30, 30, 30, 30, 40, 40],
+  //           widths: [30, '*', 150, 150],
+  //           headerRows: 1,
+  //           // keepWithHeaderRows: 1,
+  //           dontBreakRows: true,
+
+  //           body: this.pdfContent,
+  //           style: 'lineSpacing24',
+  //         },
+  //       },
+  //       { text: '', style: 'lineSpacing60' },
+  //     ],
+  //     // pageMargins: [ this.inchToPointConvert(0.7), this.inchToPointConvert(0.8), this.inchToPointConvert(0.2), this.inchToPointConvert(0.4) ],
+  //     footer: function (currentPage, pageCount) {
+  //       return {
+  //         margin: 0,
+  //         columns: [
+  //           {
+  //             // fontSize: 9,
+  //             text: [
+  //               {
+  //                 text: 'HR Transfer System',
+  //                 // style: 'footer1',
+  //                 margin: [2, 0, 0, 0],
+  //               },
+  //             ],
+  //             // alignment: 'center'
+  //           },
+  //           {
+  //             // fontSize: 9,
+  //             text: [
+  //               {
+  //                 text:
+  //                   pageCount > 1
+  //                     ? `${currentPage.toString()} of ${pageCount}`
+  //                     : '',
+  //                 style: 'footerPageNumber',
+  //               },
+  //             ],
+  //             alignment: 'center',
+  //           },
+  //           {
+  //             fontSize: 9,
+  //             text: [
+  //               {
+  //                 text: '', //new Date().toLocaleDateString(),
+  //               },
+  //             ],
+  //             // alignment: 'center'
+  //           },
+  //         ],
+  //       };
+  //     },
+  //     defaultStyle: {
+  //       font: 'NIKOSH',
+  //       fontSize: 10,
+  //     },
+  //     styles: {
+  //       lineSpacing12: {
+  //         margin: [0, 0, 0, 12],
+  //       },
+  //       lineSpacing24: {
+  //         margin: [0, 0, 0, 24],
+  //         fontSize: 14,
+  //       },
+  //       lineSpacing60: {
+  //         margin: [0, 0, 0, 60],
+  //       },
+  //       tableSetup: {
+  //         margin: [-5, 0, -5, 0],
+  //         // margin: [0, 5, 0, 15]
+  //       },
+  //       headerTexts: {
+  //         fontSize: 9,
+  //         alignment: 'center',
+  //         margin: [0, 2, 0, 0],
+  //       },
+  //       // header: {
+  //       //   fontSize: 9,
+  //       //   alignment: 'right',
+  //       //   margin: [0, 5, 0, 0]
+  //       // },
+  //       tableTexts: {
+  //         fontSize: 10,
+  //         margin: [0, 3, 0, 0],
+  //       },
+  //       tableNumbers: {
+  //         fontSize: 9,
+  //         alignment: 'right',
+  //         margin: [0, 3, 0, 0],
+  //       },
+  //       footer1: {
+  //         fontSize: 6,
+  //         margin: [10, 0, 0, 0],
+  //         // alignment: 'center'
+  //       },
+  //       footerPageNumber: {
+  //         fontSize: 6,
+  //         // alignment: 'center'
+  //       },
+  //     },
+  //   };
+  // }
+
+  // inchToPointConvert(num: number) {
+  //   return num * 72;
+  // }
 }
